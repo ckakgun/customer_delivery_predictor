def13253d4e3780c8e45c41b5b6dae734fb7bde0
24-cka-ML <cerenkaya07@gmail.com>
2025-04-05 20:42:05 +0200

    setup and requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import find_packages, setup
 
+def get_requirements(file_path:str)->list[str]:
+
+    '''This function will return the list of requirements'''
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n"," ") for req in requirements]
+
+    if "-e ." in requirements:
+        requirements.remove("-e .")
+
+    return requirements
+
 setup(
     name="delivery_time_prediction",
     version="0.0.1",
     author="CerenKayaAkgün",
     author_email="cerenkaya07@gmail.com",
     packages=find_packages(),
-    install_requires=['pandas', 'numpy', 'seaborn']
+    install_requires=get_requirements('requirements.txt')
 )
