80496004dc7e8cf40141fc74ea033fc1a7bc9f38
24-cka-ML <cerenkaya07@gmail.com>
2025-04-23 09:38:53 +0200

    Remove Dockerfile and update requirements.txt

--- src/utils.py
+++ src/utils.py
@@ -60,14 +60,8 @@ def evaluate_models(x_train, y_train,x_test,y_test,models,param):
 
             #model.fit(X_train, y_train)  # Train model
 
-            y_train_pred = model.predict(x_train)
-
             y_test_pred = model.predict(x_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
-
             report[list(models.keys())[i]] = test_model_score
 
         return report
